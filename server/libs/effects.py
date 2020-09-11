21aed19b79047ad4cfed72b47fddbd0e2801f397
Patrick Schlachter <patrick.schlachter@fs-ing.de>
2020-09-11 23:20:00 +0200

    Test fix 2

--- server/libs/effects.py
+++ server/libs/effects.py
@@ -411,7 +411,7 @@ class Effects():
         # Get the config of the current effect
         effect_config = self._config["effects"]["effect_single"]
         # Build an empty array
-        output_array = np.zeros((3, self._config["device_config"]["LED_Count"]))
+        output_array = np.zeros((4, self._config["device_config"]["LED_Count"]))
         
         # Fill the array with the selected color
         output_array[0][:]=self._config_colours[effect_config["color"]][0]
